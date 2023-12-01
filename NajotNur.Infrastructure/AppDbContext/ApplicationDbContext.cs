827a602f73084a0562827f975e98d575dcfe8fd5
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-01 19:02:31 +0500

    APIGAteway Project

--- NajotNur.Infrastructure/AppDbContext/ApplicationDbContext.cs
+++ NajotNur.Infrastructure/AppDbContext/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using NajotNur.Domain.Models;
 
 namespace NajotNur.Infrastructure
@@ -7,7 +9,25 @@ namespace NajotNur.Infrastructure
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
         {
-            Database.Migrate();
+
+            //Database.Migrate();
+            var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+            try
+            {
+                if (databaseCreator is null)
+                {
+                    throw new Exception();
+                }
+
+                if (!databaseCreator.CanConnect())
+                    databaseCreator.CreateAsync();
+                    if (!databaseCreator.HasTables())
+                    databaseCreator.CreateTablesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public DbSet<User> Users { get; set; }
